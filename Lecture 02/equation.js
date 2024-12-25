6ca8e8a0998a586144dd5a3a99570670a660d153
Chaichana <ccnyoungboy@gmail.com>
2024-12-25 22:13:38 +0700

    xxx

---
+++ Lecture 02/equation.js
@@ -0,0 +1,8 @@
+function equation(str) {
+    return eval(str);
+}
+
+// ตัวอย่างการใช้งาน
+console.log(equation("1+1"));          // → 2
+console.log(equation("7*4-2"));        // → 26
+console.log(equation("1+1+1+1+1"));   // → 5
